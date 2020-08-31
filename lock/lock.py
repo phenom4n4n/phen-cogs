e3af3aa51f5b6eeda063e7b08058d473612b500c
PhenoM4n4n <manan18singh@gmail.com>
2020-08-31 14:05:53 -0700

    [lock] roles

--- lock/lock.py
+++ lock/lock.py
@@ -28,30 +28,31 @@ class Lock(commands.Cog):
     @checks.bot_has_permissions(manage_channels=True)
     @checks.admin_or_permissions(manage_channels=True)
     @commands.group(invoke_without_command=True)
-    async def lock(self, ctx, channel: Optional[Union[discord.TextChannel, discord.VoiceChannel]] = None):
-        """Lock a channel."""
+    async def lock(self, ctx, *, channel: Optional[Union[discord.TextChannel, discord.VoiceChannel]] = None, role: Optional[discord.Role] = None):
+        """Lock a channel. Provide a role if you would like to unlock it for that role."""
         if not channel:
             channel = ctx.channel
-
+        if not role:
+            role = ctx.guild.default_role
         if isinstance(channel, discord.TextChannel):
-            current_perms = channel.overwrites_for(ctx.guild.default_role)
+            current_perms = channel.overwrites_for(role)
             if current_perms.send_messages == False:
-                return await ctx.send(f"{channel.mention} is already locked.")
+                return await ctx.send(f"{channel.mention} is already locked for `{role}`.")
             current_perms.update(send_messages=False)
-            await channel.set_permissions(ctx.guild.default_role, overwrite=current_perms)
+            await channel.set_permissions(role, overwrite=current_perms)
             await ctx.tick()
         elif isinstance(channel, discord.VoiceChannel):
-            current_perms = channel.overwrites_for(ctx.guild.default_role)
+            current_perms = channel.overwrites_for(role)
             if current_perms.connect == False:
-                return await ctx.send(f"{channel.mention} is already locked.")
+                return await ctx.send(f"{channel.mention} is already locked for `{role}`.")
             current_perms.update(connect=False)
-            await channel.set_permissions(ctx.guild.default_role, overwrite=current_perms)
+            await channel.set_permissions(role, overwrite=current_perms)
             await ctx.tick()
 
     @checks.bot_has_permissions(manage_roles=True)
     @checks.admin_or_permissions(manage_roles=True)
     @lock.command(name="server")
-    async def lock_server(self, ctx, role: Optional[discord.Role] = None):
+    async def lock_server(self, ctx, *, role: Optional[discord.Role] = None):
         """Lock the server. Provide a role if you would like to lock it for that role."""
         if not role:
             role = ctx.guild.default_role
@@ -67,30 +68,31 @@ class Lock(commands.Cog):
     @checks.bot_has_permissions(manage_channels=True)
     @checks.admin_or_permissions(manage_channels=True)
     @commands.group(invoke_without_command=True)
-    async def unlock(self, ctx, channel: Optional[Union[discord.TextChannel, discord.VoiceChannel]] = None):
-        """Unlock a channel."""
+    async def unlock(self, ctx, *, channel: Optional[Union[discord.TextChannel, discord.VoiceChannel]] = None, role: Optional[discord.Role] = None):
+        """Unlock a channel. Provide a role if you would like to unlock it for that role."""
         if not channel:
             channel = ctx.channel
-
+        if not role:
+            role = ctx.guild.default_role
         if isinstance(channel, discord.TextChannel):
-            current_perms = channel.overwrites_for(ctx.guild.default_role)
+            current_perms = channel.overwrites_for(role)
             if current_perms.send_messages != False:
-                return await ctx.send(f"{channel.mention} is already unlocked.")
+                return await ctx.send(f"{channel.mention} is already unlocked for `{role}`.")
             current_perms.update(send_messages=None)
-            await channel.set_permissions(ctx.guild.default_role, overwrite=current_perms)
+            await channel.set_permissions(role, overwrite=current_perms)
             await ctx.tick()
         elif isinstance(channel, discord.VoiceChannel):
-            current_perms = channel.overwrites_for(ctx.guild.default_role)
+            current_perms = channel.overwrites_for(role)
             if current_perms.connect != False:
-                return await ctx.send(f"{channel.mention} is already locked.")
+                return await ctx.send(f"{channel.mention} is already locked for `{role}`.")
             current_perms.update(connect=False)
-            await channel.set_permissions(ctx.guild.default_role, overwrite=current_perms)
+            await channel.set_permissions(role, overwrite=current_perms)
             await ctx.tick()
 
     @checks.bot_has_permissions(manage_roles=True)
     @checks.admin_or_permissions(manage_roles=True)
     @unlock.command(name="server")
-    async def unlock_server(self, ctx, role: Optional[discord.Role] = None):
+    async def unlock_server(self, ctx, *, role: Optional[discord.Role] = None):
         """Unlock the server. Provide a role if you would like to unlock it for that role."""
         if not role:
             role = ctx.guild.default_role
