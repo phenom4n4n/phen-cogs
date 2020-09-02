23510af70da380dae5618193ea50298343186cb7
PhenoM4n4n <manan18singh@gmail.com>
2020-09-02 14:41:51 -0700

    [plaguegame] i forgot how to count ig

--- plaguegame/plague.py
+++ plaguegame/plague.py
@@ -161,8 +161,7 @@ class Plague(commands.Cog):
                 userState = await self.config.user(user).gameState()
                 if userState == "infected":
                     infected_list.append(user.mention)
-        infected_list = infected_list[:97]
-        embedDescription = "\n".join(infected_list)
+        embedDescription = "\n".join(infected_list[:93])
         embed = discord.Embed(title="Infected Users", description=embedDescription)
         await ctx.send(embed=embed)
 
