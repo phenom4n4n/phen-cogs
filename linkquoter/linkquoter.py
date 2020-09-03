90270e8da7bc2b219c7c6c24521a665dc206f843
PhenoM4n4n <manan18singh@gmail.com>
2020-09-02 20:45:17 -0700

    ..

--- linkquoter/linkquoter.py
+++ linkquoter/linkquoter.py
@@ -89,7 +89,7 @@ class LinkQuoter(commands.Cog):
         return embeds
 
     @commands.cooldown(3, 15, type=commands.BucketType.channel)
-    @checks.guild_only()
+    @commands.guild_only()
     @commands.group(invoke_without_command=True, aliases=["linkmessage"])
     async def linkquote(self, ctx, link: str):
         """Quote a message from a link."""
