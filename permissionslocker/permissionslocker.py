dcaccf015eaf348559384cd1328a17a0e49d1cd6
PhenoM4n4n <phenom4n4n@gmail.com>
2020-09-26 23:10:02 -0700

    add permissions value

--- permissionslocker/permissionslocker.py
+++ permissionslocker/permissionslocker.py
@@ -66,7 +66,7 @@ class PermissionsLocker(commands.Cog):
             e = discord.Embed(color=await ctx.embed_color(), title="PermissionsLocker")
             e.add_field(
                 name="Required Permissions",
-                value=box(
+                value=str(data["permissions"]) + box(
                     await self.humanize_perms(discord.Permissions(data["permissions"]), True),
                     "diff",
                 ),
