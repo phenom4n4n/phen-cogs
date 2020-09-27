b91046fe8228073de7f83cec66ad5e3507c07a1e
PhenoM4n4n <phenom4n4n@gmail.com>
2020-09-26 23:21:05 -0700

    oops

--- permissionslocker/__init__.py
+++ permissionslocker/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .permissionslocker import PermissionsLocker, before_invoke_hook
+from .permissionslocker import PermissionsLocker
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
