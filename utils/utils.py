8b6842f08b2783b7bca1466911692c4ce180234d
{{ github.actor }} <phenom4n4n@users.noreply.github.com>
2020-09-29 04:00:43 +0000

    Style Reformatting

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 from redbot.core import commands
 
+
 async def tick(ctx: commands.Context):
     emoji = ctx.bot.get_emoji(729914495459393587) or "\N{WHITE HEAVY CHECK MARK}"
     await ctx.react_quietly(emoji)
