9a30f4c19b9b1783b80d6c3696913ff2355755cc
{{ github.actor }} <phenom4n4n@users.noreply.github.com>
2020-10-10 07:04:06 +0000

    Style Reformatting

--- tags/blocks/command.py
+++ tags/blocks/command.py
@@ -1,13 +1,15 @@
-from TagScriptEngine import Interpreter, adapter
-from TagScriptEngine.interface import Block
 from typing import Optional
 
-class AssignmentBlock(Block):
-    def will_accept(self, ctx : Interpreter.Context) -> bool:
-        dec = ctx.verb.declaration.lower()
-        return any([dec=="c", dec=="com", dec=="command"])
+from TagScriptEngine import Interpreter, adapter
+from TagScriptEngine.interface import Block
 
-    def process(self, ctx : Interpreter.Context) -> Optional[str]:
+
+class AssignmentBlock(Block):
+    def will_accept(self, ctx: Interpreter.Context) -> bool:
+        dec = ctx.verb.declaration.lower()
+        return any([dec == "c", dec == "com", dec == "command"])
+
+    def process(self, ctx: Interpreter.Context) -> Optional[str]:
         if ctx.verb.parameter == None:
             return None
         ctx.response.variables[ctx.verb.parameter] = adapter.StringAdapter(str(ctx.verb.payload))
