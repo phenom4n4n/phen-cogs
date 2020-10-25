e4de847812e1390b091e2ed5fa5cd41ab0871b38
{{ github.actor }} <phenom4n4n@users.noreply.github.com>
2020-10-25 04:03:12 +0000

    Style Reformatting

--- webhook/__init__.py
+++ webhook/__init__.py
@@ -6,5 +6,6 @@ from .webhook import Webhook
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
+
 def setup(bot):
     bot.add_cog(Webhook(bot))
