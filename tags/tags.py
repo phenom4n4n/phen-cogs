0c045ff6408674c66d9d10b485ee42d4b3288c20
PhenoM4n4n <phenom4n4n@gmail.com>
2020-10-26 02:08:17 -0700

    log

--- tags/tags.py
+++ tags/tags.py
@@ -21,7 +21,7 @@ from .adapters import MemberAdapter, TextChannelAdapter, GuildAdapter
 
 RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
-log = logging.getLogger("redbot.phenom4n4n.tags")
+log = logging.getLogger("red.phenom4n4n.tags")
 
 async def delete_quietly(message: discord.Message):
     try:
@@ -262,7 +262,7 @@ class Tags(commands.Cog):
             new_message = copy(message)
             new_message.content = f"{ctx.prefix}tag False {tag_command}"
             if self.bot.user.id in [741074175875088424, 462364255128256513]: # dev stuff lol
-                print(f"Processing tag for {tag_name} on {message.guild}")
+                log.info(f"Processing tag for {tag_name} on {message.guild}")
             await self.bot.process_commands(new_message)
 
     async def process_tag(
