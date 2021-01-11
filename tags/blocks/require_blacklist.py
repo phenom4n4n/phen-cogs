13749e3ca882847916447d9698ded76e6da62b70
PhenoM4n4n <phenom4n4n@gmail.com>
2021-01-10 20:55:29 -0800

    require blacklist docs

--- tags/blocks/require_blacklist.py
+++ tags/blocks/require_blacklist.py
@@ -6,15 +6,25 @@ from TagScriptEngine.interface import Block
 
 class RequireBlock(Block):
     """
-    The require block will attempt to convert the given parameter into a channel or role, using name or ID. If the user running the tag is not in the targeted channel or doesn't have the targeted role, the tag will stop processing and it will send the response if one is given. Multiple role or channel requirements can be given, and should be split by a ",".
+    The require block will attempt to convert the given parameter into a channel 
+    or role, using name or ID. If the user running the tag is not in the targeted 
+    channel or doesn't have the targeted role, the tag will stop processing and 
+    it will send the response if one is given. Multiple role or channel 
+    requirements can be given, and should be split by a ",".
 
-    Usage: ``{require(<role,channel>):[response]}``
+    **Usage:** ``{require(<role,channel>):[response]}``
 
-    Aliases: ``whitelist``
+    **Aliases:** ``whitelist``
 
-    Payload: response, None
+    **Payload:** response, None
 
-    Parameter: role, channel
+    **Parameter:** role, channel
+
+    **Usage:** ::
+
+        {require(Moderator)}
+        {require(#general, #bot-cmds):This tag can only be run in #general and #bot-cmds.}
+        {require(757425366209134764, 668713062186090506, 737961895356792882):You aren't allowed to use this tag.}
     """
 
     def will_accept(self, ctx: Interpreter.Context) -> bool:
@@ -28,7 +38,7 @@ class RequireBlock(Block):
         if actions:
             return None
         ctx.response.actions["requires"] = {
-            "items": ctx.verb.parameter.split(","),
+            "items": [i.strip() for i in ctx.verb.parameter.split(",")],
             "response": ctx.verb.payload,
         }
         return ""
@@ -36,13 +46,23 @@ class RequireBlock(Block):
 
 class BlacklistBlock(Block):
     """
-    Same usage and syntax as the require block, but instead of requiring the given channel or roles, it will block using the tag with those roles or in those channels.
+    The blacklist block will attempt to convert the given parameter into a channel 
+    or role, using name or ID. If the user running the tag is in the targeted 
+    channel or has the targeted role, the tag will stop processing and 
+    it will send the response if one is given. Multiple role or channel 
+    requirements can be given, and should be split by a ",".
 
     Usage: ``{blacklist(<role,channel>):[response]}``
 
     Payload: response, None
 
     Parameter: role, channel
+
+    **Usage:** ::
+
+        {blacklist(Muted)}
+        {blacklist(#support):This tag is not allowed in #support.}
+        {blacklist(Tag Blacklist, 668713062186090506):You are blacklisted from using tags.}
     """
 
     def will_accept(self, ctx: Interpreter.Context) -> bool:
@@ -56,7 +76,7 @@ class BlacklistBlock(Block):
         if actions:
             return None
         ctx.response.actions["blacklist"] = {
-            "items": ctx.verb.parameter.split(","),
+            "items": [i.strip() for i in ctx.verb.parameter.split(",")],
             "response": ctx.verb.payload,
         }
         return ""
