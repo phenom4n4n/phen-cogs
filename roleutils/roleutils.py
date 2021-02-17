1a08a8f9e50fb6e155047d1f79540318b54ff812
phenom4n4n <phenom4n4n@gmail.com>
2021-02-17 01:58:40 -0800

    version bump

--- roleutils/roleutils.py
+++ roleutils/roleutils.py
@@ -36,7 +36,7 @@ class RoleUtils(
     Includes massroling, role targeting, and reaction roles.
     """
 
-    __version__ = "1.2.2"
+    __version__ = "1.2.3"
 
     def format_help_for_context(self, ctx):
         pre_processed = super().format_help_for_context(ctx)
