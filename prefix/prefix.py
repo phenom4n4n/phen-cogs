6f0093d6c9aa507ef29575d2d861cf36a705d030
PhenoM4n4n <phenom4n4n@gmail.com>
2021-05-04 12:59:57 -0700

    refactor

--- prefix/prefix.py
+++ prefix/prefix.py
@@ -21,11 +21,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
+from typing import List
 import re
 
 import discord
-from redbot.core import Config, checks, commands
+from redbot.core import Config, commands
 
 
 class Prefix(commands.Cog):
@@ -39,78 +39,118 @@ class Prefix(commands.Cog):
         return
 
     @property
-    def mention_re(self):
+    def mentionable(self) -> bool:
+        return self.bot._cli_flags.mentionable
+
+    @property
+    def mention_re(self) -> re.Pattern:
         if not self.MENTION_RE:
-            self.MENTION_RE = re.compile(rf"<@!?{self.bot.user.id}>")
+            self.MENTION_RE = re.compile(rf"^<@!?{self.bot.user.id}>$")
         return self.MENTION_RE
 
-    @checks.bot_has_permissions(embed_links=True)
+    @commands.bot_has_permissions(embed_links=True)
     @commands.guild_only()
-    @commands.group()
-    async def prefix(self, ctx):
-        """Prefix management."""
+    @commands.group(invoke_without_command=True)
+    async def prefix(self, ctx: commands.Context):
+        """
+        Manage server prefixes.
+        
+        Running this command without subcommands will show this server's prefixes.
 
-        if not ctx.subcommand_passed:
-            embed = await self.gen_prefixes(ctx)
-            await ctx.send(embed=embed)
+        **Example:**
+        `[p]prefix`
+        """
+        embed = await self.prefix_embed(ctx)
+        await ctx.send(embed=embed)
 
-    @checks.admin_or_permissions(manage_guild=True)
-    @prefix.command(name="set")
-    async def set_prefix(self, ctx, *, prefix: str):
-        """Set the prefix for this server."""
+    @commands.admin_or_permissions(manage_guild=True)
+    @prefix.command(name="set", aliases=["="], require_var_positional=True)
+    async def prefix_set(self, ctx: commands.Context, *prefixes: str):
+        """
+        Set the prefixes for this server.
+        
+        Multiple prefixes can be set at once.
+        To add a prefix with spaces, use quotes.
+        This will overwrite any current prefixes.
 
-        await self.bot.set_prefixes(guild=ctx.guild, prefixes=[prefix])
-        embed = await self.gen_prefixes(ctx)
-        await ctx.send("Prefix set.", embed=embed)
+        **Examples:**
+        `[p]prefix set ! n!`
+        `[p]prefix set .. & "Hey siri, "`
+        """
+        await self.bot.set_prefixes(guild=ctx.guild, prefixes=prefixes)
+        embed = await self.prefix_embed(ctx)
+        es = "es" if len(prefixes) > 1 else ""
+        await ctx.send(f"Prefix{es} set.", embed=embed)
 
-    @checks.admin_or_permissions(manage_guild=True)
-    @prefix.command(name="add")
-    async def add_prefix(self, ctx, *, prefix: str):
-        """Add a prefix for this server."""
+    @commands.admin_or_permissions(manage_guild=True)
+    @prefix.command(name="add", aliases=["+"])
+    async def prefix_add(self, ctx: commands.Context, prefix: str):
+        """
+        Add a prefix to this server's prefix list.
+        
+        Use quotes to add a prefix with spaces.
 
-        prefixes = await self.bot.get_valid_prefixes(ctx.guild)
+        **Examples:**
+        `[p]prefix add ?`
+        `[p]prefix + "[botname], can you please "`
+        """
+        prefixes = await self.get_prefixes(ctx.guild)
         if prefix in prefixes:
             return await ctx.send("That is already a prefix.")
-
-        if self.bot._cli_flags.mentionable:
-            prefixes = [p for p in prefixes if not self.mention_re.match(p)]
+    
         prefixes.append(prefix)
-        await ctx.bot.set_prefixes(guild=ctx.guild, prefixes=prefixes)
-        embed = await self.gen_prefixes(ctx)
+        await self.bot.set_prefixes(guild=ctx.guild, prefixes=prefixes)
+        embed = await self.prefix_embed(ctx)
         await ctx.send("Prefix added.", embed=embed)
 
-    @checks.admin_or_permissions(manage_guild=True)
-    @prefix.command(name="remove")
-    async def remove_prefix(self, ctx, *, prefix: str):
-        """Remove a prefix for this server."""
+    @commands.admin_or_permissions(manage_guild=True)
+    @prefix.command(name="remove", aliases=["-"])
+    async def prefix_remove(self, ctx: commands.Context, prefix: str):
+        """
+        Remove a prefix from this server's prefix list.
+        
+        Use quotes to remove a prefix with spaces.
 
-        prefixes = await self.bot.get_valid_prefixes(ctx.guild)
+        **Examples:**
+        `[p]prefix remove ~`
+        `[p]prefix - "Alexa, "`
+        """
+        prefixes = await self.get_prefixes(ctx.guild)
         if prefix not in prefixes:
             return await ctx.send("That is not a valid prefix.")
         if len(prefixes) == 1:
             return await ctx.send("If you removed that prefix, you would have none left.")
 
-        if self.bot._cli_flags.mentionable:
-            prefixes = [p for p in prefixes if not self.mention_re.match(p)]
-        index = prefixes.index(prefix)
-        prefixes.pop(index)
+        prefixes.remove(prefix)
         await self.bot.set_prefixes(guild=ctx.guild, prefixes=prefixes)
-        embed = await self.gen_prefixes(ctx)
+        embed = await self.prefix_embed(ctx)
         await ctx.send("Prefix removed.", embed=embed)
 
-    @checks.admin_or_permissions(manage_guild=True)
+    @commands.admin_or_permissions(manage_guild=True)
     @prefix.command(name="clear", aliases=["reset"])
-    async def reset_prefixes(self, ctx):
-        """Reset the prefixes for this server."""
+    async def prefix_clear(self, ctx: commands.Context):
+        """
+        Reset this server's prefixes to the default list.
 
+        This cannot be undone.
+
+        **Example:**
+        `[p]prefix clear`        
+        """
         await self.bot.set_prefixes(guild=ctx.guild, prefixes=[])
-        embed = await self.gen_prefixes(ctx)
+        embed = await self.prefix_embed(ctx)
         await ctx.send(f"Reset this server's prefixes.", embed=embed)
 
-    async def gen_prefixes(self, ctx: commands.Context):
+    async def get_prefixes(self, guild: discord.Guild) -> List[str]:
+        prefixes = await self.bot.get_valid_prefixes(guild)
+        if self.mentionable:
+            prefixes = [p for p in prefixes if not self.mention_re.match(p)]
+        return prefixes
+
+    async def prefix_embed(self, ctx: commands.Context) -> discord.Embed:
         prefixes = []
         for p in await self.bot.get_valid_prefixes(ctx.guild):
-            if self.bot._cli_flags.mentionable and self.mention_re.match(p):
+            if self.mentionable and self.mention_re.match(p):
                 p = p.replace("!", "")
                 if p in prefixes:
                     continue
