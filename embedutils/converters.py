91a2ab4d7c57d0a0e97e13fc7c4ccf7db4886b4a
sravan <57430307+sravan1946@users.noreply.github.com>
2021-09-17 12:30:39 +0530

    Embed pastebin (#93)
    * pastebin commands
    
    * edit commandname and docstings
    
    * oop
    
    * move converters to a mixin, add help info for pastebin
    
    Co-authored-by: PhenoM4n4n <phenom4n4n@gmail.com>

--- embedutils/converters.py
+++ embedutils/converters.py
@@ -1,12 +1,14 @@
 import asyncio
 import json
 from typing import Dict, List, Optional, Union
+import re
 
 import discord
 import yaml
 from redbot.core import commands
 from redbot.core.utils import menus
 
+PASTEBIN_RE = re.compile(r"(?:https?://(?:www\.)?)?pastebin\.com/(?:raw/)?([a-zA-Z0-9]+)")
 
 class StringToEmbed(commands.Converter):
     def __init__(
@@ -201,3 +203,24 @@ class MessageableChannel(commands.TextChannelConverter):
                 f"You do not have permissions to send embeds in {channel.mention}."
             )
         return channel
+
+
+class PastebinMixin:
+    async def convert(self, ctx: commands.Context, argument: str) -> str:
+        match = PASTEBIN_RE.match(argument)
+        if not match:
+            raise commands.BadArgument(f"`{argument}` is not a valid Pastebin link.")
+        paste_id = match.group(1)
+        async with ctx.cog.session.get(f"https://pastebin.com/raw/{paste_id}") as resp:
+            if resp.status != 200:
+                raise commands.BadArgument(f"`{argument}` is not a valid Pastebin link.")
+            embed_data = await resp.text()
+        return await super().convert(ctx, embed_data)
+
+
+class PastebinConverter(PastebinMixin, StringToEmbed):
+    ...
+
+
+class PastebinConverterWebhook(PastebinMixin, ListStringToEmbed):
+    ...
