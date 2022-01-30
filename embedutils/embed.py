f2105da6c4d7bc5fac7d788dff916364dd1e2e21
PhenoM4n4n <61065078+phenom4n4n@users.noreply.github.com>
2022-01-30 15:43:28 -0800

    [repo] update license info

--- embedutils/embed.py
+++ embedutils/embed.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2020-2021 phenom4n4n
+Copyright (c) 2020-present phenom4n4n
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
