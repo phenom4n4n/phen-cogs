ec9f72f2355e46ba119434067ed37162bea31413
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-08-03 03:25:05 +0530

    oops typo, my bad

--- tags/processor.py
+++ tags/processor.py
@@ -176,7 +176,7 @@ class Processor(MixinMeta):
 
             if actions.get("commands"):
                 for command in actions["commands"]:
-                    if commands.split()[0] == "invoketag":
+                    if command.split()[0] == "invoketag":
                         await ctx.send("Tag looping isn't allowed.")
                         return
                     new = copy(ctx.message)
