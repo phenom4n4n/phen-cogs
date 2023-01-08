5419075e2113e9d27568cdc88f8c2dffee2c5421
Lemon Rose <yash.kul69@gmail.com>
2023-01-08 22:51:08 +0530

    [PfpImgen] Fix `ctx.trigger_typing()`

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -85,25 +85,25 @@ class PfpImgen(commands.Cog):
     @commands.command(cooldown_after_parsing=True)
     async def bonk(self, ctx, *, member: FuzzyMember = None):
         """Bonk! Go to horny jail."""
-        await ctx.trigger_typing()
-        bonker = False
-        if member:
-            bonker = ctx.author
-        else:
-            member = ctx.author
-
         async with ctx.typing():
-            victim_avatar = await self.get_avatar(member)
-            if bonker:
-                bonker_avatar = await self.get_avatar(bonker)
-                task = functools.partial(self.gen_bonk, ctx, victim_avatar, bonker_avatar)
+            bonker = False
+            if member:
+                bonker = ctx.author
             else:
-                task = functools.partial(self.gen_bonk, ctx, victim_avatar)
-            image = await self.generate_image(task)
-        if isinstance(image, str):
-            await ctx.send(image)
-        else:
-            await ctx.send(file=image)
+                member = ctx.author
+
+            async with ctx.typing():
+                victim_avatar = await self.get_avatar(member)
+                if bonker:
+                    bonker_avatar = await self.get_avatar(bonker)
+                    task = functools.partial(self.gen_bonk, ctx, victim_avatar, bonker_avatar)
+                else:
+                    task = functools.partial(self.gen_bonk, ctx, victim_avatar)
+                image = await self.generate_image(task)
+            if isinstance(image, str):
+                await ctx.send(image)
+            else:
+                await ctx.send(file=image)
 
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
