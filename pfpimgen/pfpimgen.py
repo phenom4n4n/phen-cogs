5ae2ec4ffbff6284c10e13a34bde7528395b6299
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2023-03-30 01:11:15 +0200

    [PfpImgen] fix bonk command

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -85,7 +85,7 @@ class PfpImgen(commands.Cog):
     @commands.command(cooldown_after_parsing=True)
     async def bonk(self, ctx, *, member: FuzzyMember = None):
         """Bonk! Go to horny jail."""
-        await ctx.trigger_typing()
+        await ctx.typing()
         bonker = False
         if member:
             bonker = ctx.author
